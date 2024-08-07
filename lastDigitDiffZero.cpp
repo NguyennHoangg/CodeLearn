f3ab2036057fdb2728db954592f0673632e52931
Nguyễn Huy Hoàng <139753465+NguyennHoangg@users.noreply.github.com>
2024-08-07 23:43:32 +0700

    Update lastDigitDiffZero.cpp

--- lastDigitDiffZero.cpp
+++ lastDigitDiffZero.cpp
@@ -1,3 +1,25 @@
+
+/* Tìm chữ số khác không cuối cùng của n!(giai thừa)
+
+Ví dụ:
+
+Với n = 5, kết quả lastDigitDiffZero(n) = 2.
+5! = 1 · 2 · 3 · 4 · 5 = 120.
+Với n = 6, kết quả lastDigitDiffZero(n) = 2.
+6! =1 · 2 · 3 · 4 · 5 · 6 = 720.
+Với n = 10, kết quả lastDigitDiffZero(n) = 8.
+10! = 3628800.
+Đầu vào/Đầu ra
+
+[Thời gian chạy] 0.5 giây
+[input] integer n
+Điều kiện:
+1 ≤ n ≤ 10^6.
+[output] integer
+Chữ số cuối cùng khác 0 của n! */
+
+
+
 #include<iostream> 
 using namespace std;
 
